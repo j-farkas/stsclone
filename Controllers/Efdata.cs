32f36817b5e4109b058c7566bb188752070ed656
Jared Farkas <jaredmfarkas@gmail.com>
2019-07-25 09:58:02 -0700

    Add database model

--- Controllers/Efdata.cs
+++ Controllers/Efdata.cs
@@ -7,20 +7,57 @@ namespace my_new_app.Context
 
 	public class DataContext : DbContext
 	{
-		public DbSet<Stuff> Stuff { get; set; }	
+		public DbSet<Card> Card { get; set; }
+		public DbSet<Enemy> Enemy { get; set; }
+		public DbSet<Attack> Attack { get; set;}
+		public DbSet<Relic> Relic { get; set;}
+		public DbSet<Event> Event { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseMySQL("server=localhost;database=overreaction;user=root;password=root;port=8889;");
-		}		
+			optionsBuilder.UseMySQL("server=localhost;database=stsclone;user=root;password=root;port=8889;");
+		}
 	}
 
-	public class Stuff
+	public class Card
 	{
 		public int Id { get; set; }
-		public int Day { get; set; }
-		public int Season { get; set; }
-		public int Year { get; set; }		
+		public int Cost { get; set; }
+		public string Type { get; set; }
+		public int Value { get; set; }
+		public int Effects { get; set; }
+		public int Color { get; set; }
+	}
+
+	public class Enemy
+	{
+		public int Id { get; set; }
+		public List<Attack> Attacks { get; set; }
+		public int RewardClass { get; set; }
+		public int HP { get; set; }
+	}
+
+	public class Attack
+	{
+		public int Id { get; set; }
+		public string Type { get; set; }
+		public int Value { get; set; }
+		public int Effects { get; set; }
+	}
+
+	public class Relic
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Value { get; set; }
+		public string Effects {get; set; }
+	}
+
+	public class Event
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public int RewardClass { get; set; }
 	}
 
 }
---
+++ Migrations/20190725165716_initialcreate.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace my_new_app.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Card",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Cost = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(nullable: true),
+                    Value = table.Column<int>(nullable: false),
+                    Effects = table.Column<int>(nullable: false),
+                    Color = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Card", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enemy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    RewardClass = table.Column<int>(nullable: false),
+                    HP = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enemy", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    RewardClass = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Relic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Value = table.Column<int>(nullable: false),
+                    Effects = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relic", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attack",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Type = table.Column<string>(nullable: true),
+                    Value = table.Column<int>(nullable: false),
+                    Effects = table.Column<int>(nullable: false),
+                    EnemyId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attack", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attack_Enemy_EnemyId",
+                        column: x => x.EnemyId,
+                        principalTable: "Enemy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attack_EnemyId",
+                table: "Attack",
+                column: "EnemyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attack");
+
+            migrationBuilder.DropTable(
+                name: "Card");
+
+            migrationBuilder.DropTable(
+                name: "Event");
+
+            migrationBuilder.DropTable(
+                name: "Relic");
+
+            migrationBuilder.DropTable(
+                name: "Enemy");
+        }
+    }
+}
